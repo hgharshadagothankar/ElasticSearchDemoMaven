a5bca2d37dd9e0eb4ef3be0ffd6406ef97036eb3
Harshada gothankar <hgharshadagothankar@gmail.com>
2017-04-08 10:39:56 -0400

    demo commit

---
+++ src/main/java/org/springframework/data/elasticsearch/core/mapping/SimpleElasticsearchPersistentEntity.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2013-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.elasticsearch.core.mapping;
+
+import static org.springframework.util.StringUtils.*;
+
+import java.util.Locale;
+import java.util.Optional;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.expression.BeanFactoryAccessor;
+import org.springframework.context.expression.BeanFactoryResolver;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Parent;
+import org.springframework.data.elasticsearch.annotations.Setting;
+import org.springframework.data.mapping.model.BasicPersistentEntity;
+import org.springframework.data.util.TypeInformation;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.util.Assert;
+
+/**
+ * Elasticsearch specific {@link org.springframework.data.mapping.PersistentEntity} implementation holding
+ *
+ * @param <T>
+ * @author Rizwan Idrees
+ * @author Mohsin Husen
+ * @author Mark Paluch
+ */
+public class SimpleElasticsearchPersistentEntity<T> extends BasicPersistentEntity<T, ElasticsearchPersistentProperty>
+		implements ElasticsearchPersistentEntity<T>, ApplicationContextAware {
+
+	private final StandardEvaluationContext context;
+	private final SpelExpressionParser parser;
+
+	private String indexName;
+	private String indexType;
+	private boolean useServerConfiguration;
+	private short shards;
+	private short replicas;
+	private String refreshInterval;
+	private String indexStoreType;
+	private Optional<String> parentType = Optional.empty();
+	private Optional<ElasticsearchPersistentProperty> parentIdProperty = Optional.empty();
+	private String settingPath;
+	private boolean createIndexAndMapping;
+
+	public SimpleElasticsearchPersistentEntity(TypeInformation<T> typeInformation) {
+		super(typeInformation);
+		this.context = new StandardEvaluationContext();
+		this.parser = new SpelExpressionParser();
+
+		Class<T> clazz = typeInformation.getType();
+		if (clazz.isAnnotationPresent(Document.class)) {
+			Document document = clazz.getAnnotation(Document.class);
+			Assert.hasText(document.indexName(),
+					" Unknown indexName. Make sure the indexName is defined. e.g @Document(indexName=\"foo\")");
+			this.indexName = document.indexName();
+			this.indexType = hasText(document.type()) ? document.type() : clazz.getSimpleName().toLowerCase(Locale.ENGLISH);
+			this.useServerConfiguration = document.useServerConfiguration();
+			this.shards = document.shards();
+			this.replicas = document.replicas();
+			this.refreshInterval = document.refreshInterval();
+			this.indexStoreType = document.indexStoreType();
+			this.createIndexAndMapping = document.createIndex();
+		}
+		if (clazz.isAnnotationPresent(Setting.class)) {
+			this.settingPath = typeInformation.getType().getAnnotation(Setting.class).settingPath();
+		}
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		context.addPropertyAccessor(new BeanFactoryAccessor());
+		context.setBeanResolver(new BeanFactoryResolver(applicationContext));
+		context.setRootObject(applicationContext);
+	}
+
+	@Override
+	public String getIndexName() {
+		Expression expression = parser.parseExpression(indexName, ParserContext.TEMPLATE_EXPRESSION);
+		return expression.getValue(context, String.class);
+	}
+
+	@Override
+	public String getIndexType() {
+		Expression expression = parser.parseExpression(indexType, ParserContext.TEMPLATE_EXPRESSION);
+		return expression.getValue(context, String.class);
+	}
+
+	@Override
+	public String getIndexStoreType() {
+		return indexStoreType;
+	}
+
+	@Override
+	public short getShards() {
+		return shards;
+	}
+
+	@Override
+	public short getReplicas() {
+		return replicas;
+	}
+
+	@Override
+	public boolean isUseServerConfiguration() {
+		return useServerConfiguration;
+	}
+
+	@Override
+	public String getRefreshInterval() {
+		return refreshInterval;
+	}
+
+	@Override
+	public Optional<String> getParentType() {
+		return parentType;
+	}
+
+	@Override
+	public Optional<ElasticsearchPersistentProperty> getParentIdProperty() {
+		return parentIdProperty;
+	}
+
+	@Override
+	public String settingPath() {
+		return settingPath;
+	}
+
+	@Override
+	public boolean isCreateIndexAndMapping() {
+		return createIndexAndMapping;
+	}
+
+	@Override
+	public void addPersistentProperty(ElasticsearchPersistentProperty property) {
+		super.addPersistentProperty(property);
+
+		Optional<Parent> annotation = property.findAnnotation(Parent.class);
+
+		annotation.ifPresent(parent -> {
+			Assert.isTrue(!this.parentIdProperty.isPresent(), "Only one field can hold a @Parent annotation");
+			Assert.isTrue(!this.parentType.isPresent(), "Only one field can hold a @Parent annotation");
+			Assert.isTrue(property.getType() == String.class, "Parent ID property should be String");
+			this.parentIdProperty = Optional.of(property);
+			this.parentType = Optional.of(parent.type());
+		});
+
+		if (property.isVersionProperty()) {
+			Assert.isTrue(property.getType() == Long.class, "Version property should be Long");
+		}
+	}
+}
